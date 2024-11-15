6a70b9f56fd83d8172e0b2cd187bbec6cdb445d0
dastudillom <daniela.astudillo@ucsf.edu>
2024-11-15 08:51:45 -0800

    Updated script

--- get_annots.py
+++ get_annots.py
@@ -19,8 +19,8 @@ from get_annots_tools import LoadNKData, GetDataDisconnects, GetStimulation
 
 # User-specified inputs #
 patient_id = 'PR06'
-stim_on = True
-disconnects_on = False
+stim_on = False
+disconnects_on = True
 
 INPUT_PATH = None
 OUTPUT_PATH = '/userdata/dastudillo/patient_data/stage1/'
@@ -31,12 +31,12 @@ concatenated_annots = LoadNKData(patient_id, INPUT_PATH)
 if stim_on == True:
     stim_annots = GetStimulation(concatenated_annots)
     fn_stim = f'{patient_id}_Stage1_StimulationParameters.csv'
-    stim_annots.to_csv(pathlib.Path(OUTPUT_PATH,fn_stim))
+    stim_annots.to_csv(pathlib.Path(OUTPUT_PATH,fn_stim), index=False)
 
 if disconnects_on == True:
     disconnects_annots = GetDataDisconnects(concatenated_annots)
     fn_disc = f'{patient_id}_Stage1_DataDisconnects.csv'
-    disconnects_annots.to_csv(pathlib.Path(OUTPUT_PATH,fn_disc))
+    disconnects_annots.to_csv(pathlib.Path(OUTPUT_PATH,fn_disc), index=False)
 
 
 """End of code"""
